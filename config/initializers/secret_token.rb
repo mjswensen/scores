10a18b6d9e181079b0fed1227387ea70beff5612
Matt Swensen <mjs@lucidchart.com>
2014-03-24 11:39:31 -0600

    Setting up with required Gems for our stack.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scores::Application.config.secret_key_base = 'a1c7800fd5e91c50fccccad1ca928e41711a3f0f4e351c982aa5fb739991f7eabf63f3545a0c13336f99b2db54c0086c1abe303a6d0bd42c85849abc1f8bea54'
