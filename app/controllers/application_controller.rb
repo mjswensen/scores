f7230595682a38e9ce09a0b7c4104e3c14b88b7c
Matt Swensen <mjs@lucidchart.com>
2014-03-26 09:39:40 -0600

    Configuring the root Ruby route.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+  	# The action that responds to the user's initial request.
+  	# The view for this action will serve up all of our Angular code.
+  end
 end
