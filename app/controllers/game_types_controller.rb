6b400f37bbb065a9675aed4fbc1c926625378283
Matt Swensen <mjs@lucidchart.com>
2014-04-09 20:38:29 -0600

    Quick changes to prep the work for the game types controller.

--- app/controllers/game_types_controller.rb
+++ app/controllers/game_types_controller.rb
@@ -3,9 +3,9 @@ class GameTypesController < ApplicationController
   	# Should return data for all game types.
   end
   def show
-  	# Should return the data for the give game type id.
+  	# Should return the data for the given game type id.
   end
-  def edit
+  def update
   	# Should accept new data for given game type, alter it (save it) and return it.
   end
   def create
