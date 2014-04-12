2e770ce482f0aec7dc5dd8b9b8b12a744032b9b4
Roy Matsunaga <roytmatsunaga@gmail.com>
2014-04-12 16:07:26 -0600

    added css, js, and images

---
+++ app/assets/javascripts/modal.js
@@ -0,0 +1,35 @@
+$(function()
+{
+	$('#add_player').on('click', function ()
+	{
+		$('#modal').show();
+	});
+
+	// $('#modal').on('click', function ()
+	// {
+	// 	$('#modal').hide();
+	// });
+
+	$('#cancel').on('click', function ()
+	{
+		$('#modal').hide();
+	});
+
+	$(window).resize(function ()
+	{
+		resizeModal();
+	});
+
+	resizeModal();
+
+	function resizeModal()
+	{
+		var dTop = (window.innerHeight - $('#dialogue').height()) / 3.0;
+		var dLeft = (window.innerWidth - $('#dialogue').width()) / 2.0;
+
+		$('#dialogue').css("top", dTop + "px");
+		$('#dialogue').css("left", dLeft + "px");
+	}
+});
+
+
