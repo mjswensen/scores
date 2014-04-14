94541edac478873940a90b6d50dd079451ab28cf
Matt Swensen <mjs@lucidchart.com>
2014-04-14 17:43:41 -0600

    Form resets after game submission.

--- app/assets/javascripts/scores_ajax.js
+++ app/assets/javascripts/scores_ajax.js
@@ -1,17 +1,17 @@
 $(function ()
 {
-	
+
 	//populates the game types dropdowns
-	$.getJSON( "game_types", function(json) 
+	$.getJSON( "game_types", function(json)
 	{
 		var values = [];
 
-	  	json.forEach(function (item) 
+	  	json.forEach(function (item)
 	  	{
 	  		values.push('<option value="' + item.id + '">' + item.name + '</option>');
 	  	});
 
-	  	var html = values.join("");		
+	  	var html = values.join("");
 
 	  	$("#game_type").html(html);
 	  	$("#game_type_leader").html(html);
@@ -19,24 +19,24 @@ $(function ()
 	  	console.log( "game_type load success" );
 	})
 
-	.fail( function () 
+	.fail( function ()
 	{
 		console.log( "game_types failed to load" );
 	});
 
 	// populates the players dropdowns
-	$.getJSON( "players", function(json) 
+	$.getJSON( "players", function(json)
 	{
 		var values = [];
 
 		values.push('<option value="" disabled selected>Select your option...</option>');
 
-	  	json.forEach(function (item) 
+	  	json.forEach(function (item)
 	  	{
 	  		values.push('<option value="' + item.id + '">' + item.first_name + ' \"' + item.nickname + '\" ' + item.last_name + '</option>');
 	  	});
 
-	  	var html = values.join("");		
+	  	var html = values.join("");
 
 	  	$("#player1").html(html);
 	  	$("#player2").html(html);
@@ -44,22 +44,22 @@ $(function ()
 	  	console.log( "players load success" );
 	})
 
-	.fail( function () 
+	.fail( function ()
 	{
 		console.log( "players failed to load" );
 	});
 
 	// populates the ranking table
-	// $.getJSON( "players", function(json) 
+	// $.getJSON( "players", function(json)
 	// {
 	// 	var values = [];
 
-	//   	json.forEach(function (item) 
+	//   	json.forEach(function (item)
 	//   	{
 	//   		values.push('<option value="' + item.id + '">' + item.first_name + ' \"' + item.nickname + '\" ' + item.last_name + '</option>');
 	//   	});
 
-	//   	var html = values.join("");		
+	//   	var html = values.join("");
 
 	//   	$("#player1").html(html);
 	//   	$("#player2").html(html);
@@ -67,11 +67,11 @@ $(function ()
 	//   	console.log( "players load success" );
 	// })
 
-	// .fail( function () 
+	// .fail( function ()
 	// {
 	// 	console.log( "players failed to load" );
 	// });
-	
+
 	//create a new game in the DB
 	$("#new_game_form").submit( function(event)
 	{
@@ -89,7 +89,7 @@ $(function ()
 
 		var p2 = new Object();
 		p2.score = $("#part2_score").val();
-		p2.players = [$("#player2").val()]; //TODO: will eventually need to change to add multiple players 
+		p2.players = [$("#player2").val()]; //TODO: will eventually need to change to add multiple players
 		a.push(p2);
 
 		var pars = JSON.stringify(a);
@@ -97,7 +97,7 @@ $(function ()
 		if ($("#player1").val() == $("#player2").val())
 		{
 			$('#same_id').show();
-		} 
+		}
 		else
 		{
 			$.post(game_form.attr("action"), {game_type: type, participants: pars}, function ()
@@ -111,9 +111,10 @@ $(function ()
 				});
 
 				console.log( "game created" );
+				game_form[0].reset();
 			})
 
-			.fail( function () 
+			.fail( function ()
 			{
 				alert("Game not created.  Required fields missing.");
 			});
@@ -130,7 +131,7 @@ $(function ()
 		$.post(player_form.attr("action"), {first_name: $("#first_name").val(), nickname: $("#nickname").val(), last_name: $("#last_name").val()}, function (json)
 		{
 			//TODO: get full list of names (even if some have been removed)
-			
+
 			//Adds new player to selects
 			$('#player1').append('<option value="' + json.id + '">' + json.first_name + ' \"' + json.nickname + '\" ' + json.last_name + '</option>');
 			$('#player2').append('<option value="' + json.id + '">' + json.first_name + ' \"' + json.nickname + '\" ' + json.last_name + '</option>');
@@ -146,7 +147,7 @@ $(function ()
 			console.log( "player created" );
 		})
 
-		.fail( function () 
+		.fail( function ()
 		{
 			alert("Player not created, required fields missing.");
 		});
